2167b4d1041af15a646825767b4d1feab76625aa
Kritinitya7 <kritinitya7@gmail.com>
2024-03-19 16:02:26 +0530

    version-1 Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-function App(){
-
-  return <center className = 'todo-container'>placeholder</center>
+import AppName from "./components/AppName";
+import Addtodo from "./components/Addtodo";
+import TodoItem from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2";
+import "./App.css";
+function App() {
+  return (
+    <center className="todo-container">
+      <AppName></AppName>
+      <Addtodo></Addtodo>
+      <div className="items-container">
+        <TodoItem></TodoItem>
+        <TodoItem2></TodoItem2>
+      </div>
+    </center>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TodoItem1.jsx
@@ -0,0 +1,19 @@
+function TodoItem() {
+  let todoName = "Buy Item";
+  let todoDate = "19/03/2024";
+  return (
+    <div className="container">
+      <div className="row kg-row">
+        <div className="col-6">{todoName}</div>
+        <div className="col-4">{todoDate}</div>
+        <div className="col-2">
+          <button type="button" className="btn btn-danger kg-button">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem;
